d9fa477af04309ad8e8f6873ca75dfba86e0a516
Mikołaj <kaleta222@gmail.com>
2025-06-30 15:01:32 +0200

    feat: communicate with the client

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  app.enableCors({
+    origin: ['http://localhost:8081', 'http://192.168.1.30:8081'],
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
